00dcfcd21eebe75cb80ef6230daf4f42ff3049d0
miguelkristian17 <Miguelkmartinez@gmail.com>
2021-12-20 22:47:49 -0800

    lingo changes

---
+++ index.js
@@ -0,0 +1,14 @@
+var myFullpage = new fullpage('#fullpage', {
+    //options here
+    autoScrolling:true,
+    scrollingSpeed: 700,
+    scrollHorizontally: true,
+    navigation: true,
+    fitToSection: true,
+    anchors:['firstPage', 'secondPage', 'thirdPage', 'fourthPage'],
+    navigationTooltips: ['Home', 'Bio', 'Projects', 'Contact'],
+    showActiveTooltip: true,
+    controlArrows: false,
+    slidesNavigation: true,
+    slidesNavPosition: 'bottom'
+});
